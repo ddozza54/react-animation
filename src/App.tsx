953aed174637c6e530886062608b7abe82cc6944
UHquokka <bananamayong@gmail.com>
2023-05-17 22:43:31 +0900

    스크롤바 이동에 따른 scale 변경

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,19 @@ import {
   motion,
   useMotionValue,
   useMotionValueEvent,
+  useScroll,
   useTransform,
+  useViewportScroll,
 } from "framer-motion";
-import { start } from "repl";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-const BiggeerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
@@ -43,13 +33,23 @@ const boxVariants = {
 
 export default function App() {
   const x = useMotionValue(0);
-  const potato = useTransform(x, [-800, 0, 800], [2, 1, 0]);
-  useMotionValueEvent(potato, "change", (l) => {
-    console.log(l);
+  const rotateZ = useTransform(x, [-800, 800], [-180, 180]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollYProgress } = useScroll();
+  useMotionValueEvent(scrollYProgress, "change", (x) => {
+    console.log(x);
   });
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
-    <Wrapper>
-      <Box style={{ x, scale: potato }} dragSnapToOrigin drag="x" />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, scale, rotateZ }} dragSnapToOrigin drag="x" />
     </Wrapper>
   );
 }
