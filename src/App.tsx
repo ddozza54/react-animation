81ed354553c250793564c919813c36aa678b1447
UHquokka <bananamayong@gmail.com>
2023-05-18 19:59:55 +0900

    slider chaper 1

--- src/App.tsx
+++ src/App.tsx
@@ -18,41 +18,57 @@ const Box = styled(motion.div)`
   position: absolute;
   top: 100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
 `;
 
 const showingVa = {
   initial: {
     opacity: 0,
     scale: 0,
+    x: 500,
   },
   visible: {
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    x: 0,
+    transition: { duration: 1 },
   },
   leaving: {
     opacity: 0,
-    y: 20,
+    x: -500,
+    transition: { duration: 1 },
   },
 };
 export default function App() {
-  const [showing, setShowing] = useState(false);
-  const handleClick = () => {
-    setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => {
+    setVisible((prev) => (prev === 8 ? 8 : prev + 1));
+  };
+  const prevPlease = () => {
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
   };
   return (
     <Wrapper>
       <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={showingVa}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((i) =>
+          i === visible ? (
+            <Box
+              variants={showingVa}
+              initial="initial"
+              animate="visible"
+              exit="leaving"
+              key={i}
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
-      <button onClick={handleClick}>Click</button>
+      <button onClick={prevPlease}>Prev</button>
+      <button onClick={nextPlease}>Next</button>
     </Wrapper>
   );
 }
